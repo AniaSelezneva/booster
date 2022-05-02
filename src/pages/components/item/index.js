09d9e92d1811da7b6ac404265eee908a68670cfa
AnnaS <53278654+AniaSelezneva@users.noreply.github.com>
2022-05-02 16:08:38 +0300

    Update index.js

--- src/pages/components/item/index.js
+++ src/pages/components/item/index.js
@@ -9,17 +9,21 @@ import { add_item } from "../../../context/actions"
 
 
 // markup
-const Item = ({ data: { item } }) => {
+const Item = ({ data }) => {
   const dispatch = React.useContext(GlobalDispatchContext)
   const state = React.useContext(GlobalStateContext)
+  
+  let item = undefined
+  if(data) item = item.data
 
 
   return (<li className={styles.item}>
+    { item ?
     <img src={item.img} alt={item.title}></img>
     <h3>{item.title}</h3>
     <p>{item.description}</p>
     <button onClick={() => { dispatch(add_item(item)) }}>add</button>
-    <Link to={`/items/${item.slug}`}>More info</Link>
+    <Link to={`/items/${item.slug}`}>More info</Link> :<span/>}
   </li>)
 }
 
